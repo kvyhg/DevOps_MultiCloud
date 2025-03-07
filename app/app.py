efe7f8b1e031b61d45fe59833a0867a994d2ca3f
kvyhg <137982369+kvyhg@users.noreply.github.com>
2025-03-07 16:11:18 +0000

    app commit 1 static styles.css templates base.html templates home.html templates status.html

--- app/app.py
+++ app/app.py
@@ -1,16 +1,16 @@
 from prometheus_flask_exporter import PrometheusMetrics
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)  # This exposes `/metrics` endpoint
+app = Flask(__name__, static_url_path='/static')  # Explicitly define static folder
+metrics = PrometheusMetrics(app)  # Exposes `/metrics`
 
 @app.route('/')
 def home():
-    return "Hello, Welcome to my App!"
+    return render_template("home.html")
 
 @app.route('/status')
 def status():
-    return "✅ App is running smoothly on Kubernetes."
+    return render_template("status.html", status="✅ App is running smoothly on Kubernetes.")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
